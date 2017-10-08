ae5fa4f79e863243bb42583475dcbafd0c665d99
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2017-10-08 14:13:59 +0200

    Ad functions for alignment of data in block

---
+++ type/align.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# alignment function
+from math import ceil
+
+def align(b, alignment=4):
+    """Returns bytes object aligned to specified number of bytes"""
+    full = ceil(len(b) / alignment)
+    diff = (full * alignment) - len(b)
+    return b + bytes(diff)
+
+def unalign(b, alignment=4):
+    """Returns bytes after end of alignment to specified number of bytes"""
+    return b[alignment:]
