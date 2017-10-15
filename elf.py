909e9e4d2a9fc468c12c5602f1987f93bf111ef8
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2017-10-15 15:04:56 +0200

    ELF class: basic wrapper interface
    All means of wrapping already existing functionality of low-level
    classes

---
+++ elf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# module for high-level manipulation of ELF files
+from elfstruct import *
+
+class ELF:
+    """This class is a wrapper on ELF structures provided by elfstruct module
+    
+    It provides set of functions allowing easy manipulation of ELF as a whole,
+    without requirement of update of particular fields, especially offsets and
+    section headers"""
+
+    def __init__(self, e_class=ELFCLASS.ELFCLASS32, e_data=ELFDATA.ELFDATA2MSB,
+            e_type=ET.ET_EXEC, e_machine=EM.EM_NONE):
+        if e_class == ELFCLASS.ELFCLASS32:
+            cls = Elf32
+        else:
+            raise Exception('ELF class %s currently unsupported' % e_class)
+
+        if e_data == ELFDATA.ELFDATA2MSB:
+            little = False
+        elif e_data == ELFDATA.ELFDATA2LSB:
+            little = True
+        else:
+            little = sys.byteorder == 'little'
+
+        self.Elf = cls(e_ident=Elf32_e_ident(EI_CLASS=e_class, EI_DATA=e_data),
+                e_type=e_type, e_machine=e_machine, little=little)
+
+    def __str__(self):
+        return str(self.Elf)
+
+    def __repr__(self):
+        return repr(self.Elf)
+
+    def __bytes__(self):
+        """Serialize ELF object into block of bytes"""
+        return bytes(self.Elf)
+
+    def from_bytes(b):
+        """Deserializes ELF from block of bytes"""
+        return Elf32.from_bytes(b)
