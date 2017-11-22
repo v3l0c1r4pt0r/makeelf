a35bf4c9ca789bc0874efc4917f138a4036af53a
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2017-11-22 18:58:32 +0100

    uint8: implementation

---
+++ type/uint8.py
@@ -0,0 +1,28 @@
+import struct
+
+class uint8:
+
+    def __init__(self, integer, little=False):
+        self.little = little
+        if little:
+            self._endian = '<'
+        else:
+            self._endian = '>'
+        self.integer = integer
+
+    def __bytes__(self):
+        return struct.pack("%sB" % self._endian, int(self.integer))[-1:]
+
+    def __str__(self):
+        return "%d" % self.integer
+
+    def __len__(self):
+        return len(bytes(self))
+
+    def from_bytes(b, little=False):
+        if little:
+            _endian = '<'
+        else:
+            _endian = '>'
+        integer, = struct.unpack("%sB" % _endian,b[:1])
+        return uint8(integer), b[1:]
