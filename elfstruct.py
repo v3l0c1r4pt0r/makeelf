b6b0ef41e9d121e06af5b3d532f43c6fdae6bdb0
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2017-11-22 17:45:35 +0100

    elfstruct: add section header flags definition

--- elfstruct.py
+++ elfstruct.py
@@ -544,6 +544,21 @@ class SHT(Enum):
     SHT_HIUSER = 0xffffffff
 
 
+class SHF(Enum):
+    SHF_WRITE = 0x1
+    SHF_ALLOC = 0x2
+    SHF_EXECINSTR = 0x4
+    SHF_MERGE = 0x10
+    SHF_STRINGS = 0x20
+    SHF_INFO_LINK = 0x40
+    SHF_LINK_ORDER = 0x80
+    SHF_OS_NONCONFORMING = 0x100
+    SHF_GROUP = 0x200
+    SHF_TLS = 0x400
+    SHF_MASKOS = 0x0ff00000
+    SHF_MASKPROC = 0xf0000000
+
+
 class Elf32_Shdr:
 
     def __init__(self, sh_name=0, sh_type=SHT.SHT_NULL, sh_flags=0, sh_addr=0,
