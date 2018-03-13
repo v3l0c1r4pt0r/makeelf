61d2afdb9db23fa910a33881fbb726bae18e2159
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2018-03-13 18:32:16 +0100

    Add distutils' setup file
    Will be used to produce a package for PyPI

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(
+        name = 'makeelf',
+        packages = ['makeelf'],
+        version = '0.2.0',
+        description = 'ELF reader-writer library',
+        author = 'v3l0c1r4pt0r',
+        classifiers = [
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            ],
+        )
