003226a713abf0450abb498ac57118f322ec7151
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2020-05-24 21:03:06 +0200

    First tests for Ehdr
    Covers len() and bytes() operands. Provides test vectors with objects
    and bytes.

--- makeelf/test_elfstruct.py
+++ makeelf/test_elfstruct.py
@@ -92,3 +92,52 @@ class Elf32_e_identTests(unittest.TestCase):
 
             #self.assertEqual(expected[0].little, actual[0].little, i)
             self.assertEqual(expected, actual, 'error at element {}'.format(i))
+
+
+class Elf32_EhdrTests(unittest.TestCase):
+
+    tv_bytes = [
+            b'\x7fELF\1\2\1\0\0\0\0\0\0\0\0\0' + b'\1\0\0\0\1\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x34\0\0\0\0\0\0\0\0\0\0\0',
+            b'\x7fELF\1\2\1\0\0\0\0\0\0\0\0\0' + b'\0\1\0\0\0\0\0\1\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x34\0\0\0\0\0\0\0\0\0\0',
+            b'\x7fELF\1\1\1\0\0\0\0\0\0\0\0\0' + b'\0\0\0\0\1\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x34\0\0\0\0\0\0\0\0\0\0\0',
+            b'\x7fELF\1\2\1\0\0\0\0\0\0\0\0\0' + b'\0\0\0\0\0\0\0\1\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x34\0\0\0\0\0\0\0\0\0\0',
+            b'\x7fELF\0\1\0\1\0\0\0\0\0\0\0\0' + b'\2\0\3\0\2\0\0\0\1\0\0\0\2\0\0\0\3\0\0\0\0\0\0\0\x34\0\4\0\5\0\6\0\7\0\x08\0',
+           ]
+
+    tv_obj = [
+            Elf32_Ehdr(little=True),
+            Elf32_Ehdr(),
+            Elf32_Ehdr(e_ident=Elf32_e_ident(EI_CLASS=ELFCLASS.ELFCLASS32,
+                EI_DATA=ELFDATA.ELFDATA2LSB, EI_VERSION=EV.EV_CURRENT,
+                EI_OSABI=ELFOSABI.ELFOSABI_NONE, little=True),
+                e_type=ET.ET_NONE, e_machine=EM.EM_NONE, e_version=1, e_entry=0, e_phoff=0, e_shoff=0, e_flags=0, e_ehsize=52, e_phentsize=0, e_phnum=0, e_shentsize=0, e_shnum=0, e_shstrndx=0, little=True),
+            Elf32_Ehdr(e_ident=Elf32_e_ident(EI_CLASS=ELFCLASS.ELFCLASS32,
+                EI_DATA=ELFDATA.ELFDATA2MSB, EI_VERSION=EV.EV_CURRENT,
+                EI_OSABI=ELFOSABI.ELFOSABI_NONE),
+                e_type=ET.ET_NONE, e_machine=EM.EM_NONE, e_version=1, e_entry=0, e_phoff=0, e_shoff=0, e_flags=0, e_ehsize=52, e_phentsize=0, e_phnum=0, e_shentsize=0, e_shnum=0, e_shstrndx=0),
+            Elf32_Ehdr(e_ident=Elf32_e_ident(EI_CLASS=ELFCLASS.ELFCLASSNONE,
+                EI_DATA=ELFDATA.ELFDATA2LSB, EI_VERSION=EV.EV_NONE,
+                EI_OSABI=ELFOSABI.ELFOSABI_HPUX, little=True),
+                e_type=ET.ET_EXEC, e_machine=EM.EM_386, e_version=2, e_entry=1, e_phoff=2, e_shoff=3, e_flags=0, e_ehsize=52, e_phentsize=4, e_phnum=5, e_shentsize=6, e_shnum=7, e_shstrndx=8, little=True)
+            ]
+
+    def test_len(self):
+        for i in range(len(Elf32_EhdrTests.tv_obj)):
+            tv_obj = Elf32_EhdrTests.tv_obj[i]
+
+            invector = tv_obj
+            expected = 52
+            actual = len(invector)
+
+            self.assertEqual(expected, actual, 'error at element {}'.format(i))
+
+    def test_bytes(self):
+        for i in range(len(Elf32_EhdrTests.tv_bytes)):
+            tv_bytes = Elf32_EhdrTests.tv_bytes[i]
+            tv_obj = Elf32_EhdrTests.tv_obj[i]
+
+            invector = tv_obj
+            expected = tv_bytes
+            actual = bytes(invector)
+
+            self.assertEqual(expected, actual, 'error at element {}'.format(i))
